0bbb62c5dbd0ef2d38beb02c3cb3260cf268cdc8
Prashant Kumar <prashant8nick4@gmail.com>
2026-09-17 18:39:08 +0530

    python intro

---
+++ 1-start.py
@@ -0,0 +1,11 @@
+print("hello world");
+
+# This is comment
+
+"""" Multiline comment is not available in Python.
+But we can achieve it using Doc String """
+
+# This is variable
+fullName = "Prashant Kumar";
+
+print(fullName);
