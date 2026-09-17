613751bac21bac15668c52e059dd0eb912ba2966
Prashant Kumar <prashant8nick4@gmail.com>
2026-09-17 18:40:22 +0530

    string slicing

---
+++ 3-string-slicing.py
@@ -0,0 +1,18 @@
+uniqueCode = 'a'
+print(ord(uniqueCode))
+
+characterOfUniqueCode = 65
+print(chr(characterOfUniqueCode))
+
+i = 'HELLO PYTHON'
+print(i[1])
+print(i[-2])
+
+# [start:stop:step position]
+print(i[0:5:1])
+print(i[6::1])
+
+s = "PRASHANT_KUMAR"
+print(s[0:8:3])
+print(s[-1::-1])
+
