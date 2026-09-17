0ac7bfa8b278daeec5a826f935d7dbb10e4fdd15
Prashant Kumar <prashant8nick4@gmail.com>
2026-09-17 18:41:14 +0530

    input output

---
+++ 5-input-output.py
@@ -0,0 +1,13 @@
+name = "Prashant"
+age = 24
+
+print(name, age)
+
+print("My name is", name, "and age is", age)
+
+# formatted string
+print(f"I'm {name} and my age is {age}")
+
+year = int(input("What year is this? "))
+print(type(year))
+print('hi')
