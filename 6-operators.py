761cb9348c9d5e142aa39230cd0fed5553786680
Prashant Kumar <prashant8nick4@gmail.com>
2026-09-17 18:41:28 +0530

    operators

---
+++ 6-operators.py
@@ -0,0 +1,59 @@
+# # # # # Airthmetic Operators (7) => +, -, *, /, %(remainder), //(convert to int), **(power)
+
+# # # # a = 11
+# # # # b = 2
+
+# # # # print(a + b)
+# # # # print(a - b)
+# # # # print(a * b)
+# # # # print(a % b)
+# # # # print(a / b)
+# # # # print(a // b)
+# # # # print(a**b)
+# # # # print(5**3)
+
+
+# # # # Assignment Operator
+
+# # # c = 18
+
+# # # # Compound assignment operator
+
+# # # c = c + 10
+
+# # # c = c + 12
+
+# # # c += 20
+
+# # # print(c)
+
+# # # # -=
+# # # # *=
+# # # # /=
+# # # # //=
+# # # # **=
+# # # # %=
+
+# # # Comparision Operators => >, <, >=, <=, ==, != return boolean value
+
+
+# # print(1 == 2)
+# # print(1 != 2)
+# # print(1 > 2)
+# # print(1 <= 2)
+
+# # print(ord("A"))
+# # print(ord("a"))
+# # print("A" < "B")
+# # print("Abc" > "Acd")
+
+# # # print('A' > 32) => gives error because you can't compare string and no
+
+
+# # Logical Operators => and, or, not
+
+# print(12 == 12 and 12 > 10 and 12 != 12)
+# print(12 < 12 or 12 > 19 or 12 != 12 or 12 > 5)
+# print(not 12==12)
+
+# print(True and bool(0))
