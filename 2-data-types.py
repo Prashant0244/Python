ffa6011626b33b8ce9ca955c34ae8a8d2a1cd968
Prashant Kumar <prashant8nick4@gmail.com>
2026-09-17 18:39:45 +0530

    data types

---
+++ 2-data-types.py
@@ -0,0 +1,22 @@
+# integer number
+a = 1;
+print(type(a));
+
+# float number
+
+b = -22.3;
+print(type(b));
+
+c = 3/2;
+print(type(c));
+
+# complex number
+d = 32j;
+print(type(d));
+
+st = "This is string"
+print(type(st));
+
+boolean = True;
+boolean = False;
+print(type(boolean));
