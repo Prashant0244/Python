fe77db5ef1f2431c92fc77b151693fb81b5b0932
Prashant Kumar <prashant8nick4@gmail.com>
2026-09-17 18:40:46 +0530

    type conversion

---
+++ 4-type-conversion.py
@@ -0,0 +1,28 @@
+# int() float() str() bool()
+
+# False Value (7) => false, 0, 0.0, "", (), {}, []
+
+# a = "12"
+# print(type(a))
+
+# a = int(a)
+# print(type(a))
+
+# b = 0
+# print(bool(b))
+
+# c = "hi"
+# print(bool(c))
+
+# d = "12.3"
+# print(type(d))
+# d = float(d)
+# print(d)
+# print(type(d))
+
+# e = 1/2
+# print(type(e))
+
+# f = 32/3
+# print(f)
+# print(int(f))
