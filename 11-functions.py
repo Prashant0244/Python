0c027e5db7b45a77316e4c073d2d0bc8a85fdbcb
Prashant Kumar <prashant8nick4@gmail.com>
2026-09-21 01:46:26 +0530

    function in python

---
+++ 11-functions.py
@@ -0,0 +1,41 @@
+# def hello():
+#     print('Hello')
+
+# hello()
+
+
+# def greet(name):
+#     print(f"Hi {name}")
+
+# greet('Prashant')
+
+
+# def sum(first, second):
+#     return first + second
+
+# print(sum(1, 2))
+
+# print(sum(second=2,first=4))
+
+
+# def place(city='Bokaro'):
+#     print(city)
+
+# place()
+# place('Mumbai')
+
+
+# def pallindrome(word):
+#     rev = ""
+
+#     for i in range(len(word) - 1, -1, -1):
+#         rev += word[i]
+
+#     if rev == word:
+#         return f"{word} is a pallindrome"
+#     else:
+#         return f"{word} is not a pallindrome"
+
+
+# print(pallindrome("naman"))
+# print(pallindrome("nick"))
