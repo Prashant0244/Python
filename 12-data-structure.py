1d397473d08e27810f1546826b706bdeae59f47d
Prashant Kumar <prashant8nick4@gmail.com>
2026-09-22 03:57:02 +0530

    data structure

---
+++ 12-data-structure.py
@@ -0,0 +1,3 @@
+# Data structure are used to store, organize, and manipulate data efficiently. Python provides several built-in data structures.
+
+# In python, we have 4 types of in-build data structure List, Tuple, Dictionary, Set.
