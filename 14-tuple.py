357cc2e50bd3c29c5faedc27aa300bb5a8dc6840
Prashant Kumar <prashant8nick4@gmail.com>
2026-09-22 22:15:42 +0530

    tuple

---
+++ 14-tuple.py
@@ -0,0 +1,14 @@
+t = (1, 23, 2, 19, 2, 2.5, 2, 2, 2, "hello", print())
+
+# print(t[0])
+
+# index = t.index(19)
+# print(index)
+
+# count = t.count(2)
+# print(count)
+
+# a, b, c = (1, 2, 3)
+# print(a)
+# print(b)
+# print(c)
