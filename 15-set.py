fe0875932cac2446a4775320e4560f2ed783c9e9
Prashant Kumar <prashant8nick4@gmail.com>
2026-09-23 00:35:20 +0530

    sets

---
+++ 15-set.py
@@ -0,0 +1,69 @@
+# s = {1, 2, 3, 4, 5}
+
+# print(type(s))
+
+# print(s)
+
+# a = hash("hi")
+# print(a)
+
+# b = hash(3)
+# print(b)
+
+# c = hash((233, 32, 31, 43, "hi"))
+# print(c)
+
+
+# f = {1, "hello", 2, 3, 8, 0, "hi", 4, 5, 11}
+
+# for i in f:
+#     print(i)
+
+
+# m = {1, 2, 4, 3, 6, 5}
+
+# m.add(11)
+
+# m.remove(5)
+
+# m.discard(7)
+
+# print(m)
+
+
+# o = {"hi", "bye", "shy", "die"}
+
+# o.pop()
+
+# print(o)
+
+# o.clear()
+
+# print(o)
+
+a = {1, 2, 3, 4}
+b = {3, 4, 5, 6}
+
+# union = a.union(b)
+# print(union)
+# union = a|b
+# print(union)
+
+# intersection = a.intersection(b)
+# print(intersection)
+# intersection = a&b
+# print(intersection)
+
+# difference = a.difference(b)
+# print(difference)
+# difference = a-b
+# print(difference)
+# b -= a
+# print(b)
+
+# sym = a.symmetric_difference(b)
+# print(sym)
+# sym = a^b
+# print(sym)
+
+
