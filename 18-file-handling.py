75a0029db90e24218e17c360db00c317fa6dedf2
Prashant Kumar <prashant8nick4@gmail.com>
2026-09-24 23:38:12 +0530

    file handling basic

---
+++ 18-file-handling.py
@@ -0,0 +1,16 @@
+# p = open("1-start.py")
+# print(p.read())
+
+
+# p = open(r'C:\Users\Prashant\Desktop\Python\5-input-output.py')
+# print(p.read())
+
+
+# r = open("superman.txt", "w")
+# r.write("I am writing inside this file using open function.")
+# r.close()
+
+
+# r = open("superman.txt", "a")
+# r.write(" And This is written using append")
+# r.close()
